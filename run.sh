a1d41a972e8183bc31a095f1aa6539705d47b5c2
Alexander Larsson <alexl@redhat.com>
2015-10-19 16:08:09 +0200

    Download app & runtime from github

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
+if ! test -d app; then
+    wget https://github.com/alexlarsson/xdg-app-oci/releases/download/initial/app.tar.gz
+    tar xf app.tar.gz
+fi
+
+if ! test -d runtime; then
+    wget https://github.com/alexlarsson/xdg-app-oci/releases/download/initial/runtime.tar.gz
+    tar xf runtime.tar.gz
+fi
+
 sed "s/@UID@/`id -u`/; s/@USER@/`id -un`/;" config.json.template > config.json
 sed "s/@UID@/`id -u`/; s/@USER@/`id -un`/;" runtime.json.template > runtime.json
 ./create_rootfs.sh

fb951fc8266eec4d084c8bfe72a72bae8636394f
Alexander Larsson <alexl@redhat.com>
2015-10-20 11:56:53 +0200

    replace @GID@ in templates too

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ if ! test -d runtime; then
     tar xf runtime.tar.gz
 fi
 
-sed "s/@UID@/`id -u`/; s/@USER@/`id -un`/;" config.json.template > config.json
-sed "s/@UID@/`id -u`/; s/@USER@/`id -un`/;" runtime.json.template > runtime.json
+sed "s/@UID@/`id -u`/; s/@GID@/`id -g`/; s/@USER@/`id -un`/;" config.json.template > config.json
+sed "s/@UID@/`id -u`/; s/@GID@/`id -g`/; s/@USER@/`id -un`/;" runtime.json.template > runtime.json
 ./create_rootfs.sh
 sudo rm -rf /run/opencontainer/containers/xdg-app-oci
 sudo `which runc` --debug start
